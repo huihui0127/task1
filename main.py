7d2dc906c901e8e0563cb49bc2021ddd5181877a
huihui0127 <68184393+huihui0127@users.noreply.github.com>
2022-11-15 23:03:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from train import train
 
 train()
+https://contentcenter-vali-drcn.dbankcdn.com/pvt_4/HW-EducationCenter_EducationCenterCourse_100_1/76/v3/MVa83s3GR7-bE1fA9crPXQ/T08ZR4UuT8yu9IsS7gRTtA.png
